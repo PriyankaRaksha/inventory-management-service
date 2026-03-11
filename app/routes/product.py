de8adf3aed7611fd55977524105416e44f5de555
Mahesh SN <mahesh@bcits.com>
2026-03-11 14:11:57 +0530

    Mahesh added product creation page to Inventory Management

---
+++ app/routes/product.py
@@ -0,0 +1,18 @@
+#Mahesh (Backend)
+from fastapi import APIRouter
+
+router = APIRouter()
+
+products = []
+
+@router.post("/products")
+def create_product(name:str, quantity:int):
+
+    product = {
+        "name": name,
+        "quantity": quantity
+    }
+
+    products.append(product)
+
+    return product
